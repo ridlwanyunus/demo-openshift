2734799279ecea1b36f32a23916565b7b692975f
ridlwan <ridlwan.yunus@pajak.go.id>
2020-02-20 10:42:46 +0700

    Initial Release

---
+++ src/main/java/com/openshift/test/demoopenshift/DemoOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.openshift.test.demoopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOpenshiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openshift/test/demoopenshift/DemoOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openshift.test.demoopenshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
